4dd6c66651db9e20f680664eb6190048eb27d97a
thanhtrana <92280911+thanhtrana@users.noreply.github.com>
2026-05-20 18:53:12 +0700

    commit

--- HocApi/Services/ProductService.cs
+++ HocApi/Services/ProductService.cs
@@ -53,5 +53,43 @@ namespace HocApi.Services
         }
 
 
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _productRepository.DeleteAsync(id);
+        }
+
+
+        public async Task<EditProductViewModel> GetByIdAsync(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null) return null;
+
+            return new EditProductViewModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity
+            };
+        }
+
+
+        public async Task<bool> EditProductAsync(int id, EditProductViewModel model)
+        {
+            var existingProduct = await _productRepository.GetByIdAsync(id);
+            if (existingProduct == null)
+            {
+                return false;
+            }
+
+            existingProduct.Name = model.Name;
+            existingProduct.Price = model.Price;
+            existingProduct.Quantity = model.Quantity;
+
+            return await _productRepository.UpdateAsync(existingProduct);
+        }
+
+
+
     }
 }
