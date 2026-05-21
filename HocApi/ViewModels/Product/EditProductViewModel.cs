7677c9936590089f9de793eb2a46a1331bf20915
thanhtrana <92280911+thanhtrana@users.noreply.github.com>
2026-05-21 19:00:58 +0700

    aaa

--- HocApi/ViewModels/Product/EditProductViewModel.cs
+++ HocApi/ViewModels/Product/EditProductViewModel.cs
@@ -1,17 +1,19 @@
-﻿namespace HocApi.ViewModels.Product
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HocApi.ViewModels.Product
 {
     public class EditProductViewModel
     {
         public int Id { get; set; }
-        
+
         [Required(ErrorMessage = "Tên sản phẩm không được để trống.")]
         [StringLength(100, ErrorMessage = "Tên sản phẩm không được vượt quá 100 kí tự.")]
         public string Name { get; set; }
-        
+
         [Required(ErrorMessage = "Giá sản phẩm không được để trống.")]
         [Range(0, double.MaxValue, ErrorMessage = "Giá sản phẩm phải lớn hơn hoặc bằng 0.")]
         public decimal Price { get; set; }
-        
+
         [Required(ErrorMessage = "Số lượng sản phẩm không được để trống.")]
         [Range(0, int.MaxValue, ErrorMessage = "Số lượng sản phẩm phải lớn hơn hoặc bằng 0.")]
         public int Quantity { get; set; }
